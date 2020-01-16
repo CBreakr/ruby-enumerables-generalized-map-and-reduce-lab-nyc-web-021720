55d652b51ceca1b8e28fd3b78822e2f28ee2b74b
Robert Notwicz <rob.notwicz@gmail.com>
2020-01-16 22:28:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,18 @@
-# Your Code Here
+
+def map(source)
+  new_arr = []
+  source.each do |val|
+    new_arr << yield(val)
+  end
+  return new_arr
+end
+
+# making a general-case initial value is 
+# a strange and ambiguous thing
+def reduce(source, initial = 0)
+  result = initial
+  source.each do |val|
+    result = yield(val, result)
+  end
+  return result
+end
